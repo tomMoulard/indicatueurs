2d84fca0d17c390b8e5fbbe2854583965468c5cf
tomMoulard <tom.moulard@epita.fr>
2017-06-07 21:52:27 +0800

    nothing interesting

--- indic-df.py
+++ indic-df.py
@@ -27,8 +27,42 @@ from gi.repository import AppIndicator3 as appindicator
 APPINDICATOR_ID = "indic-thermal-cpu"
 APPINDICATOR_ICON = os.path.dirname(os.path.realpath(__file__))\
                         + "/indic-df.svg"
-class programa:
+def clearData(d):
+    """
+    This will betterize/reformatize the data
+    The outout should be an array
 
+    For this particular usage : 
+    Filesystem      Size  Used Avail Use% Mounted on
+    udev            3,9G     0  3,9G   0% /dev
+    tmpfs           787M   28M  759M   4% /run
+    /dev/sda1       433G  396G   16G  97% /
+    tmpfs           3,9G   61M  3,8G   2% /dev/shm
+    tmpfs           5,0M  4,0K  5,0M   1% /run/lock
+    tmpfs           3,9G     0  3,9G   0% /sys/fs/cgroup
+    /dev/loop0       80M   80M     0 100% /snap/core/1689
+    /dev/loop1       79M   79M     0 100% /snap/core/1577
+    /dev/loop2       79M   79M     0 100% /snap/core/1441
+    tmpfs           787M  176K  787M   1% /run/user/1000
+    /dev/sr0        4,4G  4,4G     0 100% /media/tm/Dexter Saison 1
+    or
+    ['Filesystem      Size  Used Avail Use% Mounted on',
+    'udev            3,9G     0  3,9G   0% /dev',
+    'tmpfs           787M   28M  759M   4% /run',
+    '/dev/sda1       433G  396G   16G  97% /',
+    'tmpfs           3,9G   61M  3,8G   2% /dev/shm',
+    'tmpfs           5,0M  4,0K  5,0M   1% /run/lock',
+    'tmpfs           3,9G     0  3,9G   0% /sys/fs/cgroup',
+    '/dev/loop0       80M   80M     0 100% /snap/core/1689',
+    '/dev/loop1       79M   79M     0 100% /snap/core/1577',
+    '/dev/loop2       79M   79M     0 100% /snap/core/1441',
+    'tmpfs           787M  176K  787M   1% /run/user/1000',
+    '/dev/sr0        4,4G  4,4G     0 100% /media/tm/Dexter Saison 1',
+    '']
+    """
+    return d.split("\n")
+
+class programa:
     def __init__(self):
     # Create Indicator with icon and label
         # If leave empty "" then load empty icon
@@ -60,7 +94,8 @@ class programa:
         #self.item_core = Gtk.MenuItem(d)
         #self.menu.append(self.item_core)
         #coreMenu
-        data = d.split("\n")
+        # data = d.split("\n")
+        data = clearData(d)
         for line in data:
             self.menu.append(Gtk.MenuItem(line))
         # self.menu.append(Gtk.MenuItem(d))
