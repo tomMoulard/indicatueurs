b77389dc4d0d5369908ee788d8ad00a163630957
Tom Moulard <tom.moulard@epita.fr>
2020-06-24 12:21:37 +0200

    Migrate code style to Black

--- indic-df.py
+++ indic-df.py
@@ -9,8 +9,9 @@ Uses the function df
 To run simply run the command "python ./indic-thermal-cpu.py"
 """
 
-#main import to display the indicator
+# main import to display the indicator
 import gi
+
 gi.require_version("Gtk", "3.0")
 gi.require_version("AppIndicator3", "0.1")
 
@@ -25,8 +26,9 @@ from gi.repository import Gtk, Gdk, GLib, GObject
 from gi.repository import AppIndicator3 as appindicator
 
 APPINDICATOR_ID = "indic-thermal-cpu"
-APPINDICATOR_ICON = os.path.dirname(os.path.realpath(__file__))\
-                        + "/indic-df.svg"
+APPINDICATOR_ICON = os.path.dirname(os.path.realpath(__file__)) + "/indic-df.svg"
+
+
 def clearData(d):
     """
     This will betterize/reformatize the data
@@ -62,27 +64,31 @@ def clearData(d):
     """
     return d.split("\n")
 
+
 class programa:
     def __init__(self):
-    # Create Indicator with icon and label
+        # Create Indicator with icon and label
         # If leave empty "" then load empty icon
-        self.ind = appindicator.Indicator.new(APPINDICATOR_ID,\
-            APPINDICATOR_ICON,\
-            appindicator.IndicatorCategory.SYSTEM_SERVICES)
-        self.ind.set_status(appindicator.IndicatorStatus.ACTIVE) #
+        self.ind = appindicator.Indicator.new(
+            APPINDICATOR_ID,
+            APPINDICATOR_ICON,
+            appindicator.IndicatorCategory.SYSTEM_SERVICES,
+        )
+        self.ind.set_status(appindicator.IndicatorStatus.ACTIVE)  #
         self.menu_structure()
 
     # Menu structure
     def menu_structure(self):
         refresh = 60  # Refresh interval in seconds
 
-        #Get Data
+        # Get Data
         d = subprocess.check_output("df -h", shell=True)
 
         # GTK menu
-        self.menu      = Gtk.Menu()
-        self.item_time = Gtk.MenuItem("Last update: {}".\
-                format(strftime("%H:%M:%S", gmtime())))
+        self.menu = Gtk.Menu()
+        self.item_time = Gtk.MenuItem(
+            "Last update: {}".format(strftime("%H:%M:%S", gmtime()))
+        )
         self.item_exit = Gtk.MenuItem("Exit")
         self.item_exit.connect("activate", self.quit)
 
@@ -90,10 +96,9 @@ class programa:
         self.menu.append(self.item_time)
         self.menu.append(Gtk.SeparatorMenuItem())
 
-
-        #self.item_core = Gtk.MenuItem(d)
-        #self.menu.append(self.item_core)
-        #coreMenu
+        # self.item_core = Gtk.MenuItem(d)
+        # self.menu.append(self.item_core)
+        # coreMenu
         # data = d.split("\n")
         data = clearData(d)
         for line in data:
@@ -103,14 +108,15 @@ class programa:
         self.menu.append(self.item_exit)
         self.ind.set_menu(self.menu)
 
-        #show menu
+        # show menu
         self.menu.show_all()
 
         # Refresh indicator
-        GLib.timeout_add_seconds(refresh,self.menu_structure)
+        GLib.timeout_add_seconds(refresh, self.menu_structure)
 
     def quit(self, widget):
         sys.exit(0)
+
 
 if __name__ == "__main__":
     indicator = programa()
--- indic-thermal-cpu.py
+++ indic-thermal-cpu.py
@@ -10,8 +10,9 @@ To run simply run the command "python ./indic-thermal-cpu.py"
 """
 
 
-#main import to display the indicator
+# main import to display the indicator
 import gi
+
 gi.require_version("Gtk", "3.0")
 gi.require_version("AppIndicator3", "0.1")
 
@@ -26,32 +27,37 @@ from gi.repository import Gtk, Gdk, GLib, GObject
 from gi.repository import AppIndicator3 as appindicator
 
 APPINDICATOR_ID = "indic-thermal-cpu"
-APPINDICATOR_ICON = os.path.dirname(os.path.realpath(__file__))\
-                        + "/indic-thermal-cpu.svg"
-class programa:
+APPINDICATOR_ICON = (
+    os.path.dirname(os.path.realpath(__file__)) + "/indic-thermal-cpu.svg"
+)
 
+
+class programa:
     def __init__(self):
-    # Create Indicator with icon and label
+        # Create Indicator with icon and label
         # If leave empty "" then load empty icon
-        self.ind = appindicator.Indicator.new(APPINDICATOR_ID,\
-            APPINDICATOR_ICON,\
-            appindicator.IndicatorCategory.SYSTEM_SERVICES)
-        self.ind.set_status(appindicator.IndicatorStatus.ACTIVE) #
+        self.ind = appindicator.Indicator.new(
+            APPINDICATOR_ID,
+            APPINDICATOR_ICON,
+            appindicator.IndicatorCategory.SYSTEM_SERVICES,
+        )
+        self.ind.set_status(appindicator.IndicatorStatus.ACTIVE)  #
         self.menu_structure()
 
     # Menu structure
     def menu_structure(self):
-        refresh      = 5  # Refresh interval in seconds
+        refresh = 5  # Refresh interval in seconds
 
-        #Get Data
-        #Can use inxi -Fx
+        # Get Data
+        # Can use inxi -Fx
         d = subprocess.check_output("sensors", shell=True)
         d += "Batterie: " + subprocess.check_output("acpi -V", shell=True)
 
         # GTK menu
-        self.menu      = Gtk.Menu()
-        self.item_time = Gtk.MenuItem("Last update: {}".\
-                format(strftime("%H:%M:%S", gmtime())))
+        self.menu = Gtk.Menu()
+        self.item_time = Gtk.MenuItem(
+            "Last update: {}".format(strftime("%H:%M:%S", gmtime()))
+        )
         self.item_exit = Gtk.MenuItem("Exit")
         self.item_exit.connect("activate", self.quit)
 
@@ -59,10 +65,9 @@ class programa:
         self.menu.append(self.item_time)
         self.menu.append(Gtk.SeparatorMenuItem())
 
-
-        #self.item_core = Gtk.MenuItem(d)
-        #self.menu.append(self.item_core)
-        #coreMenu
+        # self.item_core = Gtk.MenuItem(d)
+        # self.menu.append(self.item_core)
+        # coreMenu
         data = d.split("\n")
         for line in data:
             self.menu.append(Gtk.MenuItem(line))
@@ -70,16 +75,17 @@ class programa:
         self.menu.append(self.item_exit)
         self.ind.set_menu(self.menu)
 
-        #show menu
+        # show menu
         # self.item_time.show()
         # self.item_exit.show()
         self.menu.show_all()
 
         # Refresh indicator
-        GLib.timeout_add_seconds(refresh,self.menu_structure)
+        GLib.timeout_add_seconds(refresh, self.menu_structure)
 
     def quit(self, widget):
         sys.exit(0)
+
 
 if __name__ == "__main__":
     indicator = programa()
